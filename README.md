8c0f9d85a93ab4ef1ffe639b91f8f2ba3efae90f
HI-Iris <41990354+HI-Iris@users.noreply.github.com>
2018-08-28 14:12:41 +1000

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,79 @@
-# Robot-simulator-coding-challenge
+# Toy Robot Simulator
+
+This is a WPF C# desktop application that simulates the movement of a robot on a 5*5 grid. The application can receive standard input or from a file as developer choose, the output including a position description in words and a map with a colored square.
+
+## Description
+
+- The application is a simulation of a toy robot moving on a square tabletop,
+  of dimensions 5 units x 5 units.
+- There are no other obstructions on the table surface.
+- The robot is free to roam around the surface of the table, but must be
+  prevented from falling to destruction. Any movement that would result in the
+  robot falling from the table must be prevented, however further valid movement commands must still be allowed.
+ 
+The application that can read in commands of the following form
+
+    PLACE X,Y,F
+    MOVE
+    LEFT
+    RIGHT
+
+- PLACE will put the toy robot on the table in position X,Y
+  and facing NORTH, SOUTH, EAST or WEST.
+- The origin (0,0) can be considered to be the SOUTH WEST most corner.
+- The first valid command to the robot is a PLACE command, after that,
+  any sequence of commands may be issued, in any order, including another
+  PLACE command. The application should discard all commands in the
+  sequence until a valid PLACE command has been executed.
+- MOVE will move the toy robot one unit forward in the direction it is currently
+  facing.
+- LEFT and RIGHT will rotate the robot 90 degrees in the specified direction
+  without changing the position of the robot.
+- Click on "REPORT" will announce the X,Y and F of the robot.
+- Any move that would cause the robot to fall must be ignored.
+
+## Example Input and Output:
+    
+a)
+
+	PLACE 0,0,NORTH
+    MOVE
+    
+Click "REPORT"
+
+	Output: 0,1,NORTH
+
+
+b)
+
+	PLACE 0,0,NORTH
+	LEFT
+
+Click "REPORT"
+	
+	Output: 0,0,WEST
+
+c)
+
+	PLACE 1,2,EAST
+	MOVE
+	MOVE
+	LEFT
+	MOVE
+
+Click "REPORT"
+
+	Output: 3,3,NORTH
+
+
+## Requirements
+- Implemented and tested using C#
+- Make sure you have .NET Framework 4.5.2 or above installed on your computer.
+
+##Getting started
+To build the project, run RobotMovement.sln in VS. The application would promote a visual window to receive the command by standard input or file.
+
+##Author
+
+Iris Hou [xinzhu.hou@gmail.com](xinzhu.hou@gmail.com)
+
---
+++ RobotMovement/MainWindow.xaml.cs
@@ -0,0 +1,258 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace RobotMovement
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        static private int xPosition = -1;
+        static private int yPosition = -1;
+        static private string direction = string.Empty;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        public static bool isPlaced(string command)
+        {
+            bool isPlaced = Regex.IsMatch(command, @"(?i)^PLACE [0-4],[0-4],(NORTH|SOUTH|EAST|WEST)$");
+            return isPlaced;
+        }
+
+        public static bool isFell()
+        {
+            if ((xPosition < 0) || (yPosition < 0))
+                return false;
+            else if ((xPosition > 4) || (yPosition > 4))
+                return false;
+            else
+                return true;
+        }
+
+        public void paint()
+        {
+            var cellName = "c" + xPosition + yPosition;
+            var cell = RobotTable.FindName(cellName) as TableCell;
+            switch (direction)
+            {
+                case "NORTH":
+                    cell.Background = Brushes.Yellow; ; break;
+                case "WEST":
+                    cell.Background = Brushes.Red; ; break;
+                case "SOUTH":
+                    cell.Background = Brushes.Green; ; break;
+                case "EAST":
+                    cell.Background = Brushes.Blue; ; break;
+            }
+        }
+
+        public void removeColor()
+        {
+            for (int i = 0; i <= 4; i++)
+            {
+                for (int j = 4; j >= 0; j--)
+                {
+                    var cellName = "c" + i + j;
+                    var cell = RobotTable.FindName(cellName) as TableCell;
+                    cell.Background = Brushes.Transparent;
+                }
+            }
+
+        }
+
+        private string move()
+        {
+            removeColor();
+            string result = string.Empty;
+            int lastX = xPosition;
+            int lastY = yPosition;
+            switch (direction)
+            {
+                case "NORTH":
+                    yPosition++; break;
+                case "WEST":
+                    xPosition--; break;
+                case "SOUTH":
+                    yPosition--; break;
+                case "EAST":
+                    xPosition++; break;
+            }
+            if (!isFell())
+            {
+                xPosition = lastX;
+                yPosition = lastY;
+                MessageBox.Show("Robot falls off the table.");
+                removeColor();
+                result = "";
+                Command.Text = "";
+            }
+            else
+            {
+                result = xPosition + "," + yPosition + "," + direction;
+                paint();
+            }
+
+            return result;
+        }
+
+        private string left()
+        {
+            removeColor();
+            string result = string.Empty;
+            switch (direction)
+            {
+                case "NORTH":
+                    direction = "WEST"; break;
+                case "WEST":
+                    direction = "SOUTH"; break;
+                case "SOUTH":
+                    direction = "EAST"; break;
+                case "EAST":
+                    direction = "NORTH"; break;
+            }
+            result = xPosition + "," + yPosition + "," + direction;
+            paint();
+            return result;
+        }
+
+        private string right()
+        {
+            removeColor();
+            string result = string.Empty;
+            switch (direction)
+            {
+                case "NORTH":
+                    direction = "EAST"; break;
+                case "EAST":
+                    direction = "SOUTH"; break;
+                case "SOUTH":
+                    direction = "WEST"; break;
+                case "WEST":
+                    direction = "NORTH"; break;
+            }
+            result = xPosition + "," + yPosition + "," + direction;
+            paint();
+            return result;
+        }
+
+        private void Report_Click(object sender, RoutedEventArgs e)
+        {
+            Output.Content = "";
+            string[] commandLines = Command.Text.Split(new char[] { '\r', '\n' }, StringSplitOptions.RemoveEmptyEntries);
+            if (commandLines.Length > 0)
+            {
+                var placed = isPlaced(commandLines[0]);
+                if (placed)
+                {
+                    char[] delimiterChars = { ',', ' ' };
+                    string[] placeCommand = commandLines[0].Split(delimiterChars);
+                    xPosition = Int32.Parse(placeCommand[1]);
+                    yPosition = Int32.Parse(placeCommand[2]);
+                    direction = placeCommand[3].ToUpper();
+
+                    if (commandLines.Length > 1)
+                    {
+                        for (int i = 1; i < commandLines.Length; i++)
+                        {
+                            switch (commandLines[i].ToUpper())
+                            {
+                                case "MOVE":
+                                    Output.Content = move();
+                                    break;
+                                case "LEFT":
+                                    Output.Content = left();
+                                    break;
+                                case "RIGHT":
+                                    Output.Content = right();
+                                    break;
+                                default:
+                                    MessageBox.Show("Please check your command.");
+                                    break;
+                            }
+                        }
+                    }
+                    else
+                    {
+                        Output.Content = xPosition + "," + yPosition + "," + direction;
+                        paint();
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Please check your command.");
+                    removeColor();
+                }
+            }
+            else
+            {
+                MessageBox.Show("Please enter your command.");
+
+            }
+        }
+
+        private void ReadFile_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog fd = new OpenFileDialog();
+            fd.Filter = "txt|*.txt";
+            if (fd.ShowDialog() == true)
+            {
+                if (fd.FileName != "")
+                {
+                    FilePath.Text = fd.FileName;
+                    FileStream fs = new FileStream(@fd.FileName, FileMode.Open, FileAccess.Read);
+                    StreamReader rd = new StreamReader(fs);
+                    rd.BaseStream.Seek(0, SeekOrigin.Begin);
+                    Command.Text = "";
+                    string s;
+                    var lines = new List<string>();
+                    while ((s = rd.ReadLine()) != null)
+                    {
+                        lines.Add(s);
+                    }
+                    rd.Close();
+                    fs.Close();
+                    if (lines.Count >= 1)
+                    {
+                        for (int i = 0; i < lines.Count; i++)
+                        {
+                            Command.Text += lines[i] + "\n";
+                        }
+                    }
+                    else
+                    {
+                        MessageBox.Show("Please enter your command.");
+                    }
+                }
+            }
+        }
+
+        private void InputMethod_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            Command.Text = "";
+            FilePath.Text = "";
+            Output.Content = "";
+            removeColor();
+        }
+    }
+}
+
+
